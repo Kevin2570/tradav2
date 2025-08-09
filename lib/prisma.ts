edac5c1163ba63b8cd04fad8878b7def12e8b3d8
Kevin2570 <Kevinatattoo@gmail.com>
2025-08-09 12:10:32 -0400

    Updated package-lock.json

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma ?? new PrismaClient();
+export const prisma = global.prisma ?? new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
